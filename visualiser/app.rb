8f9c04900e721732b4d5b81b8272bd30068210a5
Duncan McDonald <duncan.mcdonald@bbc.co.uk>
2014-01-09 09:32:26 +0000

    Some configuration for SSL connections.

--- visualiser/app.rb
+++ visualiser/app.rb
@@ -3,7 +3,14 @@ require 'haml'
 require 'rest-client'
 get '/' do
   RestClient.proxy = ENV['http_proxy']
-  response = RestClient.get "http://www.google.co.uk"
+  response = RestClient::Resource.new(
+      'http://google.com',
+      #:ssl_client_cert  =>  OpenSSL::X509::Certificate.new(File.read("")),
+      #:ssl_client_key   =>  OpenSSL::PKey::RSA.new(File.read("key.pem"), "passphrase, if any"),
+      :ssl_ca_file      =>  "../ca.pem",
+      :verify_ssl       =>  OpenSSL::SSL::VERIFY_PEER
+  ).get
 
-  haml :index
+  #haml :index
+  response.body
 end
