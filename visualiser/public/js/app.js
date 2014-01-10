415e484a60a394a9eeedc242a180aa2e39acb8a7
paulmc <paulmcnally000@gmail.com>
2014-01-10 09:20:13 +0000

    updated css and title tag

--- visualiser/public/js/app.js
+++ visualiser/public/js/app.js
@@ -1,32 +1,13 @@
 
 function Visualiser() {
 
+
 	this.restfulURL = 'http://localhost:4567';
 
 	this.test = function() {
    	   $('#messages').text('test');
 	};
 	
-	//formatting the data to go to #canvas
-	function writeToCanvas(jsonArrayOfHashes, levelName){
-		return;
-		var canvasString = $('#canvas').html();
-		canvasString += '<h2>' + levelName + '</h2>' 
- 		$.each(jsonArrayOfHashes, function(key, element) {
-			
-		canvasString += '<ul class="phases">' 
-				 + '<li>id : ' + element.id + '</li>' 
-				 + '<li> name : ' + element.name + '</li>' 
-				 + '<li> age range : ' + element.lower_age +'-' + element.upper_age + '</li>' 
-	//			 + '<li>' + element.levels + '</li>
-				 + '</ul>';
-				 //+ '<li><ul><li>' + element.levels.length + '</li></ul></li>
-        	});
-		//console.log(canvasString);
-		$('#canvas').html(canvasString);
-
-	}
-
 	this.getRestfulData = function(urlString){
 	
 	   var url = this.restfulURL + urlString;
