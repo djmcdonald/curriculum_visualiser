537e7a8a15899ad6e9bbdb0623e9b1689e06fbac
Duncan McDonald <duncan.mcdonald@bbc.co.uk>
2014-05-22 16:02:58 +0100

    Commented out API code

--- visualiser/helpers/rdf_repository.rb
+++ visualiser/helpers/rdf_repository.rb
@@ -16,30 +16,30 @@ class RDFRepository
   end
 
   def phases
-    response = RestClient::Resource.new(
-        ':endpoint',
-        :ssl_client_cert  =>  OpenSSL::X509::Certificate.new(File.read("cert.pem")),
-        :ssl_client_key   =>  OpenSSL::PKey::RSA.new(File.read("private.pem"), "password"),
-        :verify_ssl       =>  OpenSSL::SSL::VERIFY_NONE
-    ).get :content_type => 'application/rdf+json', :accept => 'application/rdf+json'
-
-    @phases = Phases.new
-
-    rdf_reader = RDF::Reader.for(:json).new(response.body)
-    graph = RDF::Graph.new
-    rdf_reader.each_statement { |statement| graph.insert statement }
-
-    query = RDF::Query.new(:level => {
-        RDF.type  => CURRICULUM.Phase,
-        RDFS.label => :label
-    })
-
-    results = query.execute(graph)
-
-    results.map { |result|
-      puts "****** #{result.label.to_s}"
-    }
-
+    # response = RestClient::Resource.new(
+    #     ':endpoint',
+    #     :ssl_client_cert  =>  OpenSSL::X509::Certificate.new(File.read("cert.pem")),
+    #     :ssl_client_key   =>  OpenSSL::PKey::RSA.new(File.read("private.pem"), "password"),
+    #     :verify_ssl       =>  OpenSSL::SSL::VERIFY_NONE
+    # ).get :content_type => 'application/rdf+json', :accept => 'application/rdf+json'
+    #
+    # @phases = Phases.new
+    #
+    # rdf_reader = RDF::Reader.for(:json).new(response.body)
+    # graph = RDF::Graph.new
+    # rdf_reader.each_statement { |statement| graph.insert statement }
+    #
+    # query = RDF::Query.new(:level => {
+    #     RDF.type  => CURRICULUM.Phase,
+    #     RDFS.label => :label
+    # })
+    #
+    # results = query.execute(graph)
+    #
+    # results.map { |result|
+    #   puts "****** #{result.label.to_s}"
+    # }
+    #
     File.read 'fixtures/pack.json'
     #@phases.json
   end
