fd1772de348e40bbd2f574bb44fef8d59a6e17a8
Duncan McDonald <duncan.mcdonald@bbc.co.uk>
2014-05-22 10:31:15 +0100

    Remove HAML.

--- visualiser/app.rb
+++ visualiser/app.rb
@@ -1,8 +1,8 @@
 require 'sinatra'
-require 'haml'
-require 'hamlbars'
 require 'helpers/level_builder'
 require 'helpers/phases_builder'
+
+set :environment, :development
 
 before do
   @rdf_repository = RDFRepository.new
@@ -11,7 +11,7 @@ before do
 end
 
 get '/' do
-  haml :index
+  erb :index
 end
 
 before "/education/*" do
@@ -26,5 +26,42 @@ get '/education/levels/:id' do |id|
 end
 
 get '/education/phases' do
-  @phases_builder.buildPhases.to_json
+  phases = %{
+  {
+  "phases": [
+    {
+      "id": "zvbc87h",
+      "name": "Primary",
+      "lower_age": 5,
+      "upper_age": 11,
+      "levels": [
+      {
+        "id": "zgckjxs",
+        "name": "Early and 1st level",
+        "description": "Early level is a phase of pre-school and primary education in Scotland, generally for pupils aged 3 to 6. First level is a phase of primary education in Scotland, generally for pupils aged 6 to 9.",
+        "lower_age": 5,
+        "upper_age": 11,
+        "programmes_of_study": [ ]
+      },
+      {
+      "id": "zvbc87h",
+      "name": "KS2",
+      "description": "Key Stage 2 is a phase of primary education for pupils aged 7 to 11 in England and Wales, or 8 to 11 in Northern Ireland.",
+      "lower_age": 7,
+      "upper_age": 11,
+      "programmes_of_study": [ ]
+      }
+  ]
+  },
+  {
+  "id": "zc9d7ty",
+  "name": "Secondary",
+  "lower_age": 11,
+  "upper_age": 18,
+  "levels": null
+  }
+  ]
+  }
+}
+  phases
 end
